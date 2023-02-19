ae1d46016a4472fa1f2e5414289ce5674eea4052
Haozhen Ni <nihzzzzz@seas.upenn.edu>
2023-02-18 21:07:32 -0500

    Assignment 3 Multi

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -9,18 +9,22 @@ class CONFIG:
 
     batch_size = 64
     num_epochs = 2
-    initial_learning_rate = 0.001
+    initial_learning_rate = 0.002
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "milestones": [30, 80],
+        "gamma": 0.5,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,20 @@
 from typing import List
+import numpy as np
+import math
 
 from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Decays the learning rate of each parameter group by gamma once the
+    number of epoch reaches one of the milestones. Notice that such decay can
+    happen simultaneously with other changes to the learning rate from outside
+    this scheduler. When last_epoch=-1, sets initial lr as lr.
+
+    """
+
+    def __init__(self, optimizer, milestones, gamma=0.1, last_epoch=-1):
         """
         Create a new scheduler.
 
@@ -13,12 +23,29 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
+        count_ms = []
+        for i in milestones:
+            temp = np.ones(len(milestones)) * i
+            count = np.sum(temp == milestones)
+            count_ms.append((str(i), count))
+        self.milestones = dict(count_ms)
+        self.gamma = gamma
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
-        # Note to students: You CANNOT change the arguments or return type of
-        # this function (because it is called internally by Torch)
+        """
+        Note to students: You CANNOT change the arguments or return type of
+        this function (because it is called internally by Torch)
 
+        """
         # ... Your Code Here ...
+
+        if self.last_epoch not in self.milestones:
+            return [group["lr"] for group in self.optimizer.param_groups]
+        return [
+            group["lr"] * self.gamma ** self.milestones[self.last_epoch]
+            for group in self.optimizer.param_groups
+        ]
+
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # return [i for i in self.base_lrs]
